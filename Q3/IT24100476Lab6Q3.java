c38dc7ac31b2d703633a55f37ce61b7e2e677bb6
IT24100476 <it24100476@my.sliit.lk>
2024-09-06 06:03:08 +0530

    IT24100476Lab6Q3

---
+++ Q3/IT24100476Lab6Q3.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class IT24100476Lab6Q3 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int sumOfSquares = 0;
+        int count = 0;
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            int number = input.nextInt();
+
+          
+            // Check if the input is -99 to terminate
+            if (number == -99) {
+                break;
+            }
+
+          
+
+            
+            if (number > 0) {
+                sumOfSquares += Math.pow(number, 2); // Simplified using Math.pow()
+                count++;
+            } else {
+                System.out.println("Invalid input. Please enter a positive integer or -99 to terminate.");
+            }
+        }
+
+        
+           System.out.println(" ");
+           System.out.println(" ");
+        
+
+        // Calculate Root Mean Square (RMS) 
+         double rms = Math.sqrt((double) sumOfSquares / count);
+         System.out.println("The Root Mean Square (RMS) is: " + rms);
+        
+                
+
+   }
+}
+                    
